a79ca2d24727b5d8fafcba129cddc8f186b2f539
Thondi <murbapas@bluewin.ch>
2023-08-28 11:59:07 +0100

    added some comments

--- fun_with_leds/led.py
+++ fun_with_leds/led.py
@@ -2,13 +2,25 @@
 
 import RPi.GPIO as GPIO
 import time
+
+# use the BCM-numbering (e.g. #18 and not 'pin 12')
 GPIO.setmode(GPIO.BCM)
+
+# don't print warnings
 GPIO.setwarnings(False)
+
+# set GPIO #18 as an output port
 GPIO.setup(18,GPIO.OUT)
+
 print("LED on")
+
+# set output level to high
 GPIO.output(18,GPIO.HIGH)
+
 time.sleep(1)
 print("LED off")
+
+# set output level to low
 GPIO.output(18,GPIO.LOW)
 
 
